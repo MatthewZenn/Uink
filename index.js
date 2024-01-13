0c545e6caec37f30812a43685bb854acc11555ea
Matthew Zenn <spyguy57@gmail.com>
2024-01-13 14:55:19 -0500

    added stuff

--- index.js
+++ index.js
@@ -1,6 +1,96 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-
 document.getElementById("close").addEventListener('click', () => {
   window.close();
 });
+
+var canvas = document.getElementById("canvas")
+const canvasContext = canvas.getContext('2d');
+const clearButton = document.getElementById('clear-button');
+const state = {
+  mousedown: false
+};
+
+// ===================
+// == Configuration ==
+// ===================
+var lineWidth = 2;
+var fillStyle = '#fff';
+var strokeStyle = '#fff';
+
+// =====================
+// == Event Listeners ==
+// =====================
+canvas.addEventListener('mousedown', handleWritingStart);
+canvas.addEventListener('mousemove', handleWritingInProgress);
+canvas.addEventListener('mouseup', handleDrawingEnd);
+canvas.addEventListener('mouseout', handleDrawingEnd);
+
+canvas.addEventListener('touchstart', handleWritingStart);
+canvas.addEventListener('touchmove', handleWritingInProgress);
+canvas.addEventListener('touchend', handleDrawingEnd);
+
+clearButton.addEventListener('click', handleClearButtonClick);
+
+// ====================
+// == Event Handlers ==
+// ====================
+function handleWritingStart(event) {
+  event.preventDefault();
+
+  const mousePos = getMosuePositionOnCanvas(event);
+  
+  canvasContext.beginPath();
+
+  canvasContext.moveTo(mousePos.x, mousePos.y);
+
+  canvasContext.lineWidth = lineWidth;
+  canvasContext.strokeStyle = strokeStyle;
+
+  canvasContext.fill();
+  
+  state.mousedown = true;
+}
+
+function handleWritingInProgress(event) {
+  event.preventDefault();
+  
+  if (state.mousedown) {
+    const mousePos = getMosuePositionOnCanvas(event);
+
+    canvasContext.lineTo(mousePos.x, mousePos.y);
+    canvasContext.stroke();
+  }
+}
+
+function handleDrawingEnd(event) {
+  event.preventDefault();
+  
+  if (state.mousedown) {
+
+    canvasContext.stroke();
+  }
+  
+  state.mousedown = false;
+}
+
+function handleClearButtonClick(event) {
+  event.preventDefault();
+  
+  clearCanvas();
+}
+
+// ======================
+// == Helper Functions ==
+// ======================
+function getMosuePositionOnCanvas(event) {
+  const clientX = event.clientX || event.touches[0].clientX;
+  const clientY = event.clientY || event.touches[0].clientY;
+  const { offsetLeft, offsetTop } = event.target;
+  const canvasX = clientX - offsetLeft;
+  const canvasY = clientY - offsetTop;
+
+  return { x: canvasX, y: canvasY };
+}
+
+function clearCanvas() {
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+}
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const {app, BrowserWindow, Menu} = electron;
 let mainWindow;
 
 app.on('ready', function(){
-    mainWindow = new BrowserWindow({width: 1200, height: 700, resizable: true, frame:false, icon: "Assets/Animate.ico"});
+    mainWindow = new BrowserWindow({width: 1200, height: 700, resizable: true, frame:false, icon: "Assets/Uink_icon.png"});
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'window.html'),
         protocol: 'file:',
